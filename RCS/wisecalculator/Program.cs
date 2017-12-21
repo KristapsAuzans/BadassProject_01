8fb3d0fb40d3c5b8d44b58ad4073360aaad1d563
KristapsAuzans <kristaps.auzans@gmail.com>
2017-12-21 19:43:00 +0200

    calvulator goes wild

---
+++ RCS/wisecalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wisecalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Izveido calkulatora objektu
+            mathParser parser;
+            parser = new mathParser();
+
+
+            //Paprasīt lietotājam ievadīt darbību
+
+            Console.WriteLine("Please enter calculation");
+            string input = Console.ReadLine();
+            //aprēķināt "1 + 5 - 4" Skaits ir 5, bet pēdējā simbola pozīcija ir 4 (Length = 5)
+            // "1+" Skaits ir 2 pēdējā pozīcija ir 1 (Length = 2)
+
+            //izsauc aprēķināšanas rezultātu
+            int result = parser.ParserMath(input);
+
+            //izvada rezultātu uz ekrāna
+            Console.WriteLine("Your Result" + result);
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ RCS/wisecalculator/mathParser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wisecalculator
+{
+    class mathParser
+    {
+        public int ParserMath(string input)
+        {
+            int result;
+            int counter = 0;
+            string firstenteredNumber = "";
+            string secondenteredNumber = "";
+            char enteredOperation = '';
+            bool operationFound = false;
+            while (counter < input.Length)
+            {
+                char symbol = input[counter];
+                if (symbol == '+')
+                {
+                    if (operationFound == true)
+                        int result = Int32.Parse(firstenteredNumber) + Int32.Parse(secondenteredNumber);
+                    return result;
+
+
+                }
+
+                enteredOperation = symbol;
+                operationFound = true;
+
+                else
+                {
+                    
+                    if (operationFound == false)
+                    {
+                        firstenteredNumber = firstenteredNumber + symbol;
+                    }   
+                    
+                    else
+                    {
+
+                    }
+
+                    firstenteredNumber = firstenteredNumber + symbol;
+
+
+                    int number;
+                    number = Int32.Parse(symbol.ToString());
+                }
+
+                counter = counter + 1;
+
+                Console.WriteLine("error");
+                return = 99999;
+            }
+        }
+
+    }
+}
